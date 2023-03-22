f5cf6567691a8c3e96fd8762fb7d1762de94fdbc
Anthony Cyr <acyr@egyde.ca>
2023-03-22 15:44:35 -0400

    test

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ import boto3
 # import _aws_login
 
 def list_instances():
+    nb_ec2 = 0
     client = boto3.client('ec2')
 
-    response = client.describe_instances(Filters=[{'Name' :'instance-state-name', 'Values': ["running"]}])
-    print(response)
+    instances = client.describe_instances(Filters=[{
+        # 'Name' :'instance-state-name', 'Values': ["running"]
+        }])
+    for instance in instances:
+        nb_ec2 += 1
+    print(instance)
 
 list_instances()
